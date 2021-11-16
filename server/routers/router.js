d652e478ba11f4fa97cd7955d9abdffd208b8a1f
Varun <lanjharav@gmail.com>
2021-11-17 00:44:43 +0530

    Api stuff done

--- server/routers/router.js
+++ server/routers/router.js
@@ -1,9 +1,31 @@
 import express from "express";
+import Post from "../models/postModel.js";
 
 const router = express.Router()
 
-router.get("/",(req,res) => {
-    res.send("I am dumb")
+router.get("/",async(req,res) => {
+    try{
+        const postsStuff = await Post.find()
+        res.json(postsStuff)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+router.post("/",async (req,res) => {
+
+    const post = req.body
+
+    const newPost = new Post(post)
+
+    try{
+        await newPost.save()
+        res.json(newPost)
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 
 export default router
