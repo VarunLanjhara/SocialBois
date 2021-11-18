bdd5cbfb7af6a9caf3f0c25da2fc87c8d59780d3
Varun <lanjharav@gmail.com>
2021-11-19 04:11:52 +0530

    hiding api keys and snackbars stuff

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ import mongoose from "mongoose";
 import express from "express"
 import cors from "cors"
 import postRouter from "./routers/router.js";
+import dotenv from "dotenv"
 
 const app = express()
+
+dotenv.config({
+    path:"./config.env"
+})
 
 app.use(cors())
 app.use(express.json())
@@ -12,7 +17,7 @@ app.use("/posts",postRouter)
 
 const PORT = 8000 | process.env.PORT
 
-const MONGO_URL = "mongodb+srv://varun:vk1923v19kum@cluster0.lkvee.mongodb.net/stuff?retryWrites=true&w=majority"
+const MONGO_URL = process.env.MONGO_URL
 
 app.listen(PORT,() => {
     mongoose.connect(MONGO_URL,() => {
