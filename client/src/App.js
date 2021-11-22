d662e59c428015307a25fea1eaf7b8c533348692
Varun <lanjharav@gmail.com>
2021-11-23 01:56:36 +0530

    Preloader stuff done

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import "./App.css";
-import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import Trending from "./pages/Trending/Trending";
-import SinglePost from "./pages/SinglePost/SinglePost";
-import NotFound from "./pages/NotFound/NotFound";
-import Profile from "./pages/Profile/Profile";
-import UpdateProfile from "./pages/UpdateProfile/UpdateProfile";
-import UsersPosts from "./pages/UserPosts/UsersPosts";
+import Preloader from "./components/Preloader/Preloader";
 
 function App() {
+  const Home = lazy(() => import("./pages/Home/Home"));
+  const Login = lazy(() => import("./pages/Login/Login"));
+  const Trending = lazy(() => import("./pages/Trending/Trending"));
+  const SinglePost = lazy(() => import("./pages/SinglePost/SinglePost"));
+  const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+  const Profile = lazy(() => import("./pages/Profile/Profile"));
+  const UpdateProfile = lazy(() =>
+    import("./pages/UpdateProfile/UpdateProfile")
+  );
+  const UsersPosts = lazy(() => import("./pages/UserPosts/UsersPosts"));
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Login />} />
-          <Route path="/trending" element={<Trending />} />
-          <Route path="/post/:postid" element={<SinglePost />} />
-          <Route path="/profile/:name" element={<Profile />} />
-          <Route path="/update_profile" element={<UpdateProfile />} />
-          <Route path="your_stuff" element={<UsersPosts />} />
-        </Routes>
+        <Suspense fallback={<Preloader />}>
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Login />} />
+            <Route path="/trending" element={<Trending />} />
+            <Route path="/post/:postid" element={<SinglePost />} />
+            <Route path="/profile/:name" element={<Profile />} />
+            <Route path="/update_profile" element={<UpdateProfile />} />
+            <Route path="your_stuff" element={<UsersPosts />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
